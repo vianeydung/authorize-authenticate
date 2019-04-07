3818595fb72986a67e652d8a78395199a25eb200
vianeydung <nguyencongtridung@gmail.com>
2019-04-07 23:56:37 +0700

    + add

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./config.json')
+const path = require('path');
 
-mongoose.connect(config.MONGO_URI, {useNewUrlParser: true})
-        .then(console.log('Connected MongoDb'))
-        .catch(console.log)
+
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// mongoose.connect(config.MONGO_URI, {useNewUrlParser: true})
+        // .then(console.log('Connected MongoDb'))
+        // .catch(console.log)
+				
 // ========== middleware parser(3rd/built-in) ==========
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-const {Users} = require('./models/users');
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// const {Users} = require('./models/users');
 
 
 // config routes
-app.use('/api/users', require('./routes/users'))
+// app.use('/api/users', require('./routes/users'))
 // app.use('/api/store', require('./routes/api/store'))
 // app.use('/api/item', require('./routes/api/item'))
 
